46565d4581ddd1c78fd7d790af3fa8c11bd54308
j-mahapatra <jagannathm.official@gmail.com>
2024-07-07 19:22:22 +0530

    setup auth middleware for blog routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,46 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign } from "hono/jwt"; 
+import { sign, verify } from "hono/jwt"; 
 
-const app = new Hono<{ Bindings: { DATABASE_URL: string, JWT_SECRET: string } }>();
+const app = new Hono<{ Bindings: { DATABASE_URL: string, JWT_SECRET: string }, Variables: { userId: string, userEmail: string } }>();
+
+app.use('/api/v1/blog/*', async (c, next) => {
+  try {
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const authHeader = c.req.header('Authorization');
+    const token = authHeader?.split(' ')?.[1];
+    
+    if (!token) {
+      return c.json({ message: 'Unauthorized access' }, 403);
+    }
+
+    const userData = await verify(token, c.env.JWT_SECRET);
+
+    if (typeof userData.id !== 'string' || typeof userData.email !== 'string') {
+      return c.json({ message: 'Unauthorized access' }, 403);
+    }
+
+    const user = await prisma.user.findUnique({ where: {
+      id: userData.id,
+      email: userData.email
+    } })
+
+    if (!user) {
+      return c.json({ message: 'Invalid credentials' }, 404);
+    }
+
+    c.set('userId', user.id);
+    c.set('userEmail', user.email);
+
+    await next();
+  } catch (error) {
+    return c.json({ message: 'Unauthorized access' }, 403);
+  }
+});
 
 // Route to create a new user
 app.post("/api/v1/user/signup", async (c) => {
