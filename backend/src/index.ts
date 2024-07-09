d786f7ca42ce6e6ff4f44dff1eade2eb4b347aa4
j-mahapatra <jagannathm.official@gmail.com>
2024-07-09 22:34:08 +0530

    add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,14 @@ import { withAccelerate } from '@prisma/extension-accelerate';
 import { sign, verify } from 'hono/jwt';
 import { userRouter } from './routes/user';
 import { blogRouter } from './routes/blog';
+import { cors } from 'hono/cors';
 
 const app = new Hono<{
   Bindings: { DATABASE_URL: string; JWT_SECRET: string };
   Variables: { userId: string; userEmail: string };
 }>();
+
+app.use('/api/*', cors());
 
 app.route('/api/v1/user', userRouter);
 app.route('/api/v1/blog', blogRouter);
