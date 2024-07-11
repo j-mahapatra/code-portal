d51d542e6a0a74bc93e3600d5b05eb35985ae354
j-mahapatra <jagannathm.official@gmail.com>
2024-07-12 02:26:18 +0530

    setup app routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import Blog from './pages/Blog';
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/blog/:id' element={<Blog />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
