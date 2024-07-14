73dfb9d700bd622104c6ae3fb43562694895af65
j-mahapatra <jagannathm.official@gmail.com>
2024-07-14 20:16:04 +0530

    modify frontend routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
-import Blog from './pages/Blog';
+import Blogs from './pages/Blogs';
+import BlogDetails from './pages/BlogDetails';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Routes>
         <Route path='/signup' element={<Signup />} />
         <Route path='/signin' element={<Signin />} />
-        <Route path='/blog/:id' element={<Blog />} />
+        <Route path='/blogs' element={<Blogs />} />
+        <Route path='/blogs/:id' element={<BlogDetails />} />
       </Routes>
     </BrowserRouter>
   );
